717b54593357decca2da4c191e74d6eb8252aacd
Sovan Bakshi <lionalsovan017@gmail.com>
2024-10-09 17:32:39 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useState } from 'react';
 import './../styles/App.css';
 
 const App = () => {
+  const [text, setText] = useState();
+  console.log(text);
+
   return (
     <div>
+      <label htmlFor='ip'>Enter your name</label>
+      <input id='ip' type='text' onChange={(e) => {
+        setText(e.target.value);
+      }}/>
+
         {/* Do not remove the main div */}
+
+      <p>{text && `Hello ${text}`}</p>
     </div>
   )
 }
